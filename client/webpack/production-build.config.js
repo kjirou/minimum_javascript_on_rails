dbce55f5b861c47e7a284ec38ea0ac007d0d2a66
kjirou <sorenariblog@gmail.com>
2019-10-05 13:13:27 +0900

    `npm run build` also generates "webpack-manifest.json"
    The "webpack-manifest.json" is similar to ".sprockets-manifest-randomhex.json".
    It contains a correct name of the "webpacked-{hash}.js" in that time.

--- client/webpack/production-build.config.js
+++ client/webpack/production-build.config.js
@@ -1,7 +1,9 @@
+const AssetsPlugin = require('assets-webpack-plugin');
 const path = require('path');
 
 const javaScriptRoot = path.join(__dirname, '../src'); // -> "{project-root}/client/src"
 const publicationRoot = path.join(__dirname, '../../public/client'); // -> "{project-root}/public/client"
+const temporaryFilesRoot = path.join(__dirname, '../tmp'); // -> "{project-root}/client/tmp"
 
 module.exports = {
   mode: 'production',
@@ -25,4 +27,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new AssetsPlugin({
+      filename: 'webpack-manifest.json',
+      path: temporaryFilesRoot,
+    }),
+  ],
 };
