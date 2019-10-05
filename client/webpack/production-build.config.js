caddc8d0361c520c424f97f8b0c6408b3099cd0c
kjirou <sorenariblog@gmail.com>
2019-10-05 12:27:38 +0900

    Define `npm run build` script
    When you execute `npm run build`, it generates a .js file to "public/client/webpacked-{hash}.js".
    The webpack runtime bundles and compiles all JavaScript sources related from "client/src/index.js" recursively.

---
+++ client/webpack/production-build.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const javaScriptRoot = path.join(__dirname, '../src'); // -> "{project-root}/client/src"
+const publicationRoot = path.join(__dirname, '../../public/client'); // -> "{project-root}/public/client"
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    'webpacked': path.join(javaScriptRoot, 'index.js'),
+  },
+  output: {
+    filename: '[name]-[chunkhash].js',
+    path: publicationRoot,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
