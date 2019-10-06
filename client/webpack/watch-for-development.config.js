4f8703b8d99e163f7c659fef1f305054e4a49d60
kjirou <sorenariblog@gmail.com>
2019-10-06 16:11:12 +0900

    Commonize webpack.config.js fields

--- client/webpack/watch-for-development.config.js
+++ client/webpack/watch-for-development.config.js
@@ -1,50 +1,15 @@
-const AssetsPlugin = require('assets-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const path = require('path');
 const webpack = require('webpack');
 
-const javaScriptRoot = path.join(__dirname, '../src'); // -> "{project-root}/client/src"
-const publicationRoot = path.join(__dirname, '../../public/client'); // -> "{project-root}/public/client"
-const temporaryFilesRoot = path.join(__dirname, '../tmp'); // -> "{project-root}/client/tmp"
+const {baseWebpackConfig, pluginCreators} = require('./shared');
 
 module.exports = {
   mode: 'none',
   watch: true,
-  entry: {
-    'webpacked': path.join(javaScriptRoot, 'index.js'),
-  },
-  output: {
-    filename: '[name]-[chunkhash].js',
-    path: publicationRoot,
-  },
+  entry: baseWebpackConfig.entry,
+  output: baseWebpackConfig.output,
   module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                [
-                  '@babel/preset-env',
-                  {
-                    // This "forceAllTransforms" transforms sources for working on many browsers as possible.
-                    //
-                    // Normally, "@babel/preset-env" performs only the minimum necessary conversions
-                    //   for the supported browsers specified by the "targets" option or the ".browserslistrc" file.
-                    // So this option slows down the transpiling and increases the file size of the built ".js".
-                    // But if you don't have to write a lot of JavaScript, you don't have to worry about it.
-                    forceAllTransforms: true,
-                  },
-                ],
-              ],
-            },
-          },
-        ],
-      },
-    ],
+    rules: baseWebpackConfig.module.rules,
   },
   plugins: [
     // This is similar to `npm run clean`.
@@ -55,9 +20,6 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.NODE_ENV': JSON.stringify('development'),
     }),
-    new AssetsPlugin({
-      filename: 'webpack-manifest.json',
-      path: temporaryFilesRoot,
-    }),
+    pluginCreators.createAssetsWebpackPlugin(),
   ],
 };
