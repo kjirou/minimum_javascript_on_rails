7287b04deacedb3bc6dd4950ff875ddeb69b751f
kjirou <sorenariblog@gmail.com>
2019-10-06 20:05:46 +0900

    Prevent the "Please execute npm run build ..." error

--- config/initializers/client_side_supporter.rb
+++ config/initializers/client_side_supporter.rb
@@ -1,10 +1,9 @@
 webpack_manifest_json_path = Rails.root.join('client', 'tmp', 'webpack-manifest.json')
 
-unless File.exist?(webpack_manifest_json_path)
-  raise 'Please execute `npm run build` command before operating the Rails.'
+if File.exist?(webpack_manifest_json_path)
+  Rails.application.config.x.client_side_supporter.webpack_manifest =
+    JSON.parse(File.read(webpack_manifest_json_path))
 end
-Rails.application.config.x.client_side_supporter.webpack_manifest =
-  JSON.parse(File.read(webpack_manifest_json_path))
 
 if Rails.application.config.x.client_side_supporter.update_webpack_manifest_on_reloading == true
   reloader = Rails.application.config.file_watcher.new([webpack_manifest_json_path]) do
